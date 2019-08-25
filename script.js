efff584628a437eb4722eebf371ad152804cc889
Zulkarnine43 <zulkarnine43@gmail.com>
2019-08-25 21:13:05 +0600

    img animations

---
+++ script.js
@@ -0,0 +1,31 @@
+ 
+$('#img1').click(function(){
+    //var imagSrcValue=this.attr('src');
+    var imagSrcValue=$('#img1').attr('src');
+    $('#mainImage').attr('src',imagSrcValue);
+
+});
+
+ 
+$('#img2').click(function(){
+    //var imagSrcValue=this.attr('src');
+    var imagSrcValue=$('#img2').attr('src');
+    $('#mainImage').attr('src',imagSrcValue);
+
+});
+ 
+$('#img3').click(function(){
+    //var imagSrcValue=this.attr('src');
+    var imagSrcValue=$('#img3').attr('src');
+    $('#mainImage').attr('src',imagSrcValue);
+
+});
+
+ 
+$('#img4').click(function(){
+    //var imagSrcValue=this.attr('src');
+    var imagSrcValue=$('#img4').attr('src');
+    $('#mainImage').attr('src',imagSrcValue);
+
+});
+
